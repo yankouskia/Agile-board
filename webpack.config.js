f5caf35740a435a5f4874c96f44b5419c67d037c
aliaksandr_yankouski <aliaksandr_yankouski@epma.com>
2015-10-05 20:00:58 +0300

    refactor

--- webpack.config.js
+++ webpack.config.js
@@ -6,46 +6,46 @@ var opener = require('opener');
 var ROOT_PATH = path.resolve(__dirname);
 
 module.exports = {
-  entry: path.resolve(ROOT_PATH, 'app/client'),
+    entry: path.resolve(ROOT_PATH, 'app/client'),
 
-  output: {
-    path: path.resolve(ROOT_PATH, 'build'),
-    filename: 'bundle.js'
-  },
+    output: {
+        path: path.resolve(ROOT_PATH, 'build/'),
+        filename: 'bundle.js'
+    },
 
-  module: {
-    loaders: [
-      { 
-      	test: path.join(__dirname, 'app/client'),
-        loader: 'babel-loader' 
-      },
-      {
-        test: /\.css$/,
-        loaders: ['style', 'css'],
-        include: path.resolve(ROOT_PATH, 'app/client')
-      }
-    ]
-  },
+    module: {
+        loaders: [
+            { 
+                test: path.join(__dirname, 'app/client'),
+                loader: 'babel-loader' 
+            },
+            {
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+                include: path.resolve(ROOT_PATH, 'app/client')
+            }
+        ]
+    },
 
-  devServer: {
-  	port: 3000,
-    historyApiFallback: true,
-    hot: true,
-    inline: true,
-    progress: true
-  },
+    devServer: {
+        port: 3000,
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true
+    },
 
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
 
-    new HtmlwebpackPlugin({
-      title: 'Agile board'
-    }),
+        new HtmlwebpackPlugin({
+            title: 'Agile board'
+        }),
 
-    function () {
-        opener('http://localhost:3000');
-    }
-  ],
+        function () {
+            opener('http://localhost:3000');
+        }
+    ],
 
-  devtool: 'source-map'
+    devtool: 'source-map'
 };
