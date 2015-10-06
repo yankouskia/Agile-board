4824fa6719e1bb358145ca1686197fc5ad5b9264
aliaksandr_yankouski <aliaksandr_yankouski@epma.com>
2015-10-06 21:00:37 +0300

    change webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 var path = require('path');
 var webpack = require('webpack');
-var opener = require('opener');
-
-var ROOT_PATH = path.resolve(__dirname);
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+// var opener = require('opener');
 
 module.exports = {
     entry: {
@@ -11,41 +10,35 @@ module.exports = {
 
     output: {
         path: path.join(__dirname, './build'),
-        filename: '[name].bundle.js'
+        filename: '[name].bundle.js',
+        chunkFilename: '[name].js'
     },
 
     module: {
         loaders: [
-            { 
-                test: path.join(__dirname, './app/client'),
-                loader: 'babel-loader' 
-            },
             {
                 test: /\.css$/,
-                loaders: ['style', 'css'],
-                include: path.join(__dirname, './app/client')
+                loader: ExtractTextPlugin.extract("style", "css-loader")
             },
             {
                 test: /\.less$/,
-                loader: "style!css!less"
+                loader: ExtractTextPlugin.extract("style", "css-loader!less-loader")
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
             }
         ]
     },
 
-    devServer: {
-        port: 3000,
-        historyApiFallback: true,
-        hot: true,
-        inline: true,
-        progress: true
-    },
-
     plugins: [
-        new webpack.HotModuleReplacementPlugin(),
+        new ExtractTextPlugin('[name].bundle.css'),
+        new webpack.HotModuleReplacementPlugin()/*,
 
         function () {
             opener('http://localhost:3000');
-        }
+        }*/
     ],
 
     devtool: 'source-map'
