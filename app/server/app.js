16217b2dfda6ff73adea6d334ec400a75ffb1fe8
aliaksandr_yankouski <aliaksandr_yankouski@epma.com>
2015-10-09 20:50:11 +0300

    refactor code

--- app/server/app.js
+++ app/server/app.js
@@ -4,26 +4,37 @@ import hbs from 'koa-hbs';
 import serve from 'koa-static';
 import mount from 'koa-mount';
 import opener from 'opener';
-var router = require('koa-router')();
-
+import compose from 'koa-compose';
+let router = require('koa-router')();
 let app = koa();
 
-app.use( hbs.middleware({
-      viewPath: __dirname + '/views'
-}));
 
-app.use(
-	mount('/assets', serve(path.join(__dirname, '../../build')))
-);
+let appSettings = compose([
+	hbs.middleware({ viewPath: __dirname + '/views' }),
+	mount('/assets', serve(path.join(__dirname, '../../build'))),
+	router.routes(),
+	router.allowedMethods(),
+	serve(__dirname)
+]);
+
+app.use(appSettings);
 
 router.get( '*', function *(next){
-
-      yield this.render('index');
+	yield this.render('index');
 })
 
-app.use(router.routes()).use(router.allowedMethods());
+// app.use(hbs.middleware({
+// 	viewPath: __dirname + '/views'
+// }));
 
-app.use(serve(__dirname));
+// app.use(
+// 	mount('/assets', serve(path.join(__dirname, '../../build')))
+// );
+
+
+// app.use(router.routes()).use(router.allowedMethods());
+
+// app.use(serve(__dirname));
 
 let server = app.listen(3000, function() {
     console.log('Koa is listening to http://localhost:3000');
