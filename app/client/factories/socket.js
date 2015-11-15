c575e2ceb7d5db890a743c8c61c0ef57c308a74f
yankouskia <aleksandr.yankovskiy@gmail.com>
2015-11-15 14:48:46 +0300

    fix to es6 factory

--- app/client/factories/socket.js
+++ app/client/factories/socket.js
@@ -1,15 +1,15 @@
 'use strict';
 import io from 'socket.io-client';
 
-export default function ($rootScope) {
-	var socket = io.connect('http://localhost:3000');
+export default ($rootScope) => {
+	let socket = io.connect('http://localhost:3000');
 	socket.on('connect', () => {
 		console.log('connected');
 	});
 	return {
 		on: function (eventName, callback) {
 			socket.on(eventName, function () {  
-					var args = arguments;
+					let args = arguments;
 					$rootScope.$apply(function () {
 						callback.apply(socket, args);
 				});
@@ -17,7 +17,7 @@ export default function ($rootScope) {
 		},
 		emit: function (eventName, data, callback) {
 			socket.emit(eventName, data, function () {
-				var args = arguments;
+				let args = arguments;
 				$rootScope.$apply(function () {
 					if (callback) {
 						callback.apply(socket, args);
