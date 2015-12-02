0d75f378a94c00f5704711a7bb4ab68133c17930
yankouskia <aleksandr.yankovskiy@gmail.com>
2015-12-02 22:31:34 +0300

    auth not work

--- app/server/app.js
+++ app/server/app.js
@@ -17,11 +17,121 @@ import socketWrapper from './middleware/socketMiddleware';
 // server logs on client
 // require('node-monkey').start({host: "127.0.0.1", port:"50500"});
 
-let router = require('koa-router')();
+let Router = require('koa-router');
 let app = koa();
 
 
+// trust proxy
+app.proxy = true;
+
+// sessions
+var session = require('koa-generic-session');
+app.keys = ['your-session-secret'];
+app.use(session());
+
+// body parser
+var bodyParser = require('koa-bodyparser');
+app.use(bodyParser());
+
+// authentication
+require('./auth');
+var passport = require('koa-passport');
+app.use(passport.initialize());
+app.use(passport.session());
+
+// append view renderer
+var views = require('koa-render');
+app.use(views('./views', {
+  map: { html: 'handlebars' },
+  cache: false
+}));
+
+
+
+var publicRoute = new Router()
+
+publicRoute.get('/login', function*() {
+	console.log('login again');
+	this.body = yield this.render('login')
+})
+
+publicRoute.post('/custom', function*(next) {
+  var ctx = this
+  yield passport.authenticate('local', function*(err, user, info) {
+    if (err) throw err
+    if (user === false) {
+      ctx.status = 401
+      ctx.body = { success: false }
+    } else {
+      yield ctx.login(user)
+      ctx.body = { success: true }
+    }
+  }).call(this, next)
+})
+
+// POST /login
+publicRoute.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+  })
+)
+
+publicRoute.get('/logout', function*(next) {
+  this.logout()
+  this.redirect('/login')
+})
+
+// publicRoute.get('/auth/facebook',
+//   passport.authenticate('facebook')
+// )
+
+// publicRoute.get('/auth/facebook/callback',
+//   passport.authenticate('facebook', {
+//     successRedirect: '/app',
+//     failureRedirect: '/'
+//   })
+// )
+
+// publicRoute.get('/auth/twitter',
+//   passport.authenticate('twitter')
+// )
+
+// publicRoute.get('/auth/twitter/callback',
+//   passport.authenticate('twitter', {
+//     successRedirect: '/app',
+//     failureRedirect: '/'
+//   })
+// )
+
+// publicRoute.get('/auth/google',
+//   passport.authenticate('google')
+// )
+
+// publicRoute.get('/auth/google/callback',
+//   passport.authenticate('google', {
+//     successRedirect: '/app',
+//     failureRedirect: '/'
+//   })
+// )
+
+app.use(publicRoute.middleware())
+
+// Require authentication for now
+app.use(function*(next) {
+  if (this.isAuthenticated()) {
+  	console.log('ffff');
+    yield next;
+  } else {
+    this.redirect('/login');
+  }
+});
+
+
+let router = new Router();
 //server settings
+
+
 let appSettings = compose([
 	favicon(__dirname + '/views/favicon/fav.ico'),
 	hbs.middleware({ viewPath: __dirname + '/views' }),
@@ -31,16 +141,22 @@ let appSettings = compose([
 	serve(__dirname)
 ]);
 
-app.use(appSettings);
 
 // REST
 routerHandler(router);
 
 // first render
-router.get('*', function *(next){
+router.get('/', function *(next){
+	console.log('234345')
 	yield this.render('index');
+	//this.body = yield this.render('index');
 })
 
+
+app.use(appSettings);
+
+
+// app.use(router.middleware())
 
 app.server = http.createServer(app.callback()).listen(3000);
 
